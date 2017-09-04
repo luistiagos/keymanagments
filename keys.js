9d3be33d3df369aa7785da936707d9508e1dc175
Luis Tiago Andrighetto Hablich - SUPDE/DECTA/DE903 <luistiago.andrighetto@gmail.com>
2017-09-04 18:45:13 -0300

    commit

--- keys.js
+++ keys.js
@@ -1,6 +1,65 @@
 var app = angular.module("appX", ['ngSanitize']);
 app.controller("appCtrl", function($scope, $sanitize) {
+  
   $scope.parse = function() {
+    if ($scope.origem == 1) {
+      $scope.parseBundlestar();
+    }
+    else {
+      $scope.parseGogobundle();
+    }  
+  }
+  
+  $scope.parseGogobundle = function() {
+    var arr =  $scope.keystext.split('\n');
+    var arrResult = [];
+    var arrDesc = [];    
+    var ret = [];
+
+    for (var i in arr) {
+      var linha = arr[i].substr(0,arr[i].search(/\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}/g)).trim();
+      var desc = linha.substr(0,linha.search(/[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}/g)).trim();
+      arrResult.push(linha);
+    }
+
+    var adicionados = []
+
+    for (var i in arrResult) {
+      if (adicionados.indexOf(arrResult[i]) == -1) {
+        var desc = arrResult[i].substr(0,arrResult[i].search(/[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}/g)).trim();
+        var repeats = $scope.getGogoRepetidos(arrResult,desc);
+        for (var j in repeats) {
+          if (!ret[j]) {
+            ret[j] = [];
+          }
+	  ret[j].push(repeats[j]);
+	  adicionados.push(repeats[j]);
+        }
+      }
+    }	
+    
+    var strRes = '';
+    for (var i=0;i<ret.length;i++) {
+      strRes += ret[i].join('\n') + '\n\n';
+    }
+
+    console.log(ret);
+    $scope.result = $sanitize(strRes);
+  }
+
+  $scope.getGogoRepetidos = function(arr, desc) {
+    var arrRes = [];
+    for (var i in arr) {
+      var descArr = arr[i].substr(0,arr[i].search(/[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}-[a-zA-Z0-9]{5}/g)).trim();
+      if (desc == descArr) {
+  	arrRes.push(arr[i]);
+      }
+    }
+   return arrRes;
+  }
+
+
+  $scope.parseBundlestar = function() {
     var arr =  $scope.keystext.split("Copy");
     var result = "";
     for (var i=0;i<arr.length;i++) {
